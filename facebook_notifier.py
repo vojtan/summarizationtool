c8d6359d2a04484e0361795b7e576a0afb86d5d5
Vojtech Nadvornik <vojtech.nadvornik@communardo.de>
2025-08-15 12:42:10 +0200

    changed loading of the token

--- facebook_notifier.py
+++ facebook_notifier.py
@@ -11,51 +11,15 @@ class FacebookNotifier:
         config = AppConfig()
         self.APP_ID = config.fb_app_id
         self.APP_SECRET = config.fb_app_secret
-        self.USER_TOKEN = config.fb_user_token  # Long-lived user token
+        self.USER_TOKEN = config.fb_user_token  
         self.PAGE_ID = config.fb_page_id
 
-        self.TOKEN_FILE = "fb_user_token.json"
 
     def load_user_token(self):
         """
-        Loads user token from local file if available, else from env.
+        Loads user token from from env.
         """
-        if os.path.exists(self.TOKEN_FILE):
-            with open(self.TOKEN_FILE, "r") as f:
-                data = json.load(f)
-            return data["access_token"], datetime.fromisoformat(data["expires_at"])
-        else:
-            # Use token from env with an assumed expiration ~60 days from now
-            return self.USER_TOKEN, datetime.utcnow() + timedelta(days=59)
-
-    def save_user_token(self, token, expires_in):
-        """
-        Saves the refreshed token and its expiry date to local file.
-        """
-        expires_at = datetime.utcnow() + timedelta(seconds=expires_in)
-        with open(self.TOKEN_FILE, "w") as f:
-            json.dump({
-                "access_token": token,
-                "expires_at": expires_at.isoformat()
-            }, f)
-
-    def refresh_user_token(self, user_token):
-        """
-        Refresh long-lived user token before it expires.
-        """
-        url = "https://graph.facebook.com/v23.0/oauth/access_token"
-        params = {
-            "grant_type": "fb_exchange_token",
-            "client_id": self.APP_ID,
-            "client_secret": self.APP_SECRET,
-            "fb_exchange_token": user_token
-        }
-        resp = requests.get(url, params=params)
-        resp.raise_for_status()
-        data = resp.json()
-        print("User token refreshed.")
-        self.save_user_token(data["access_token"], data["expires_in"])
-        return data["access_token"]
+        return self.USER_TOKEN, datetime.utcnow() + timedelta(days=59)
 
     def get_page_access_token(self, user_token, page_id):
         """
@@ -88,12 +52,7 @@ class FacebookNotifier:
         # Load user token
         user_token, expires_at = self.load_user_token()
 
-        # Refresh if expiring in <10 days
-        if (expires_at - datetime.utcnow()).days < 10:
-            print("Token close to expiry, refreshing...")
-            user_token = self.refresh_user_token(user_token)
-        else:
-            print(f" Token valid until {expires_at.isoformat()}")
+        print(f" Token valid until {expires_at.isoformat()}")
 
         page_token = self.get_page_access_token(user_token, self.PAGE_ID)
 
